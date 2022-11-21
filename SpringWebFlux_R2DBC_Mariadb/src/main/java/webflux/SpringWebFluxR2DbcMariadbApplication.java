a34d3d5fcb7c68417656a93cf09f0bd2b6f33a2e
VinhLB1997 <43948809+VinhLB1997@users.noreply.github.com>
2022-11-21 13:16:19 +0700

    init

---
+++ SpringWebFlux_R2DBC_Mariadb/src/main/java/webflux/SpringWebFluxR2DbcMariadbApplication.java
@@ -0,0 +1,13 @@
+package webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxR2DbcMariadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFluxR2DbcMariadbApplication.class, args);
+	}
+
+}
---
+++ SpringWebFlux_R2DBC_Mariadb/src/test/java/webflux/springwebflux_r2dbc_mariadb/SpringWebFluxR2DbcMariadbApplicationTests.java
@@ -0,0 +1,13 @@
+package webflux.springwebflux_r2dbc_mariadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFluxR2DbcMariadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
